b5a802f45b3d6d420e11926295af1a146d5f0b24
azamatshsh123@gmail.com <azamatshsh123@gmail.com>
2023-03-05 12:13:44 +0300

    описание метода получение объекта, фикс мелких багов

--- container.h
+++ container.h
@@ -1,50 +1,62 @@
 #pragma once
 #include<iostream>
-template<class T> class Storage {//Шаблон класса контейнера, внутренняя организация-список, внешняя-массив
+template<class T> class Storage {//Шаблон класса контейнера, внутренняя организация-массив, внешняя-массив
 public:
 	Storage() {
-
+		size = 1;
+		mas = new T[size];
 	}
 	Storage(int a) {
-
+		size = a;
+		mas = new T[size];
 	}
 	Storage(const Storage& copy) {
-
+		size = copy.size;
+		mas = new T[size];
+		for (int i = 0; i < size; ++i)
+			mas[i] = copy.mas[i];
 	}
 	~Storage() {
-
+		delete[] mas;
 	}
 	
 	void pushObject(int ind, T value);//добавление элемента
-	T deleteObject(int ind);//извлечение с удалением
-	T getObject(int ind);//извлечение без удаления
+	T* deleteObject(int ind);//извлечение с удалением
+	T* getObject(int ind);//извлечение без удаления
 	int getSize();//получение размера контейнера
 private:
-	struct Node {//структура ячейки списка
-		Node* next = NULL;
-		T object;
-	};
-	int maxsize;
-	int currentsize;
-	Node* duck;//указатель на первую ячейку в списке
+	T* mas;
+	int size;
+	void rewriteArr(int a);//метод изменения размерности массива и его перезаписи
 };
 template<class T>
 void Storage<T>::pushObject(int ind,T value) {
-
+	mas[ind] = value;
 }
 template<class T>
-T Storage<T>::deleteObject(int ind) {
+T* Storage<T>::deleteObject(int ind) {
+	if (getObject(ind) != nullptr) {
 
+
+
+
+	}
+	return nullptr;
 }
 template<class T>
-T Storage<T>::getObject(int ind) {
-
+T* Storage<T>::getObject(int ind) {
+	if (ind >= size || ind < 0)
+		return nullptr;
+	return &mas[ind];
 }
 template<class T>
 int Storage<T>::getSize() {
 
 }
+template<class T>
+void Storage<T>::rewriteArr(int a) {
 
+}
 
 
 
--- oop3.cpp
+++ oop3.cpp
@@ -1,13 +1,16 @@
 ﻿#include<iostream>
 #include"container.h"
 class test {
-
+public:
+	int t = 5;
 };
 class stest : public test {
-
+public:
+	int t = 0;
 };
 int main() {
-	Storage<test> a;
+	Storage<test> a(10);
 	stest t;
-	a.backPush(t);
+	a.pushObject(0,t);
+	std::cout << a.getObject(0)->t;
 }
